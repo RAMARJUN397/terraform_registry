4c1bb92f3715ac8e8aa1ece8c9ead0ba95bd0639
RAMARJUN397 <69568813+RAMARJUN397@users.noreply.github.com>
2021-11-15 15:25:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-south-1"
+  access_key = "AKIAUZBJNUI7NTZDR357"
+  secret_key = "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"
+
+}
+
+
 module "simple" {
   source = "./terraform-aws-rg"
   rg_name = "Bengaluru"
